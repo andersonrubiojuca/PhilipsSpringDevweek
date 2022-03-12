ebf13d54677644932e3855ee76996485507778c7
Anderson Rubio Jucá Filho <40371098+andersonrubiojuca@users.noreply.github.com>
2022-03-11 22:10:48 -0300

    Update dbcancerdemama.sql

--- dbcancerdemama.sql
+++ dbcancerdemama.sql
@@ -134,7 +134,7 @@ CREATE TABLE `regiao` (
   `total_exames` int DEFAULT NULL,
   `qnt_exames` bigint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
